c49f69ef419364a9fa5464d329cb6a4333e43dc2
Raghav Dua <duaraghav8@gmail.com>
2016-01-17 16:02:44 +0530

    Update demo.js

--- Point/demo.js
+++ Point/demo.js
@@ -1,11 +1,11 @@
 var QuadTree = require ('./lib/QuadTree')
 var myTree = new QuadTree ();
 
-myTree.insert (myTree.root, [10, 289], 'Hello worlde');
-myTree.insert (myTree.root, [10, 182], 'Helluwysgude');
-myTree.insert (myTree.root, [19, 22], 'Helluwysgude');
-myTree.insert (myTree.root, [8917, 1892], 'Helluwysgude');
-myTree.insert (myTree.root, [901, 18927], 'Helluwysgude');
+myTree.insert (myTree.root, [10, 289], 'Potter');
+myTree.insert (myTree.root, [10, 182], 'Malfoy');
+myTree.insert (myTree.root, [19, 22], 'Snape');
+myTree.insert (myTree.root, [8917, 1892], 'Lockhart');
+myTree.insert (myTree.root, [901, 18927], 'Dobby');
 
 console.log (myTree.find (myTree.root, [901, 18927]));
 console.log (myTree.find (myTree.root, [10, 299]));
